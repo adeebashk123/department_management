1c53fb38b3f305d3effb9e6d12405ec28ae72375
shaikh adeeba <124444679+adeebashk123@users.noreply.github.com>
2025-02-02 20:06:05 +0530

    Delete department_management directory

--- department_management/department/admin.py
+++
@@ -1,9 +0,0 @@
-# department/admin.py
-
-from django.contrib import admin
-from .models import Department
-
-@admin.register(Department)
-class DepartmentAdmin(admin.ModelAdmin):
-    list_display = ('dept_name', 'status')
-    search_fields = ('dept_name',)
